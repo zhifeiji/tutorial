a2225fc9bc4532214233407095b20a93d749ab4e
root <zhifeiji>
2016-04-24 23:21:55 +0800

    注释发邮件部分，增加了获取时间戳，增加了对匿名用户的处理

--- tutorial/spiders/qiushi_spider.py
+++ tutorial/spiders/qiushi_spider.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import scrapy
 from scrapy.mail import MailSender
 from scrapy.utils.project import get_project_settings
@@ -12,18 +13,20 @@ class QiushiSpider(scrapy.Spider):
 	def parse(self,response):
 		for sel in response.xpath('//div[contains(@class,"article block untagged mb15")]'):
 
-			author = sel.xpath('div[contains(@class,"author clearfix")]/a/h2/text()').extract()[0].encode('utf-8')
-			content = sel.xpath('div[contains(@class,"content")]/text()').extract()[0].encode('utf-8')
-			stats_vote = sel.xpath('div[contains(@class,"stats")]/span[contains(@class,"stats-vote")]/i/text()').extract()[0].encode('utf-8')
-			item = {"author":author,"content":content.strip(),"stats_vote":stats_vote}
+			author = sel.xpath('div[contains(@class,"author clearfix")]/a/h2/text()').extract_first(default=u'匿名用户').encode('utf-8')
+			content = sel.xpath('div[contains(@class,"content")]/text()').extract_first().encode('utf-8')
+			ctime = sel.xpath('div[contains(@class,"content")]').re(r'--(.*?)--')[0].encode('utf-8')
+			stats_vote = sel.xpath('div[contains(@class,"stats")]/span[contains(@class,"stats-vote")]/i/text()').extract_first().encode('utf-8')
+			item = {"author":author,"content":content.strip(),"stats_vote":stats_vote,"ctime":ctime}
 			self.web_data_list.append(item)
-			#print author,content,stats_vote
+			print ctime
+			print author,content,stats_vote
 	def closed(self,reason):
 		str = ''
 		for index,item in enumerate(self.web_data_list):
-			tmp = 'index:%d, author:%s, vote:%s\n[%s]\n\n' % (index,item['author'],item['stats_vote'],item['content'])
+			tmp = 'index:%d, author:%s, vote:%s,ctime:%s\n[%s]\n\n' % (index,item['author'],item['stats_vote'],item['ctime'],item['content'])
 			str = str + tmp
-		print str
-		settings = get_project_settings()
-		mailer = MailSender.from_settings(settings)
-		mailer.send(to='jeremy.h@foxmail.com',subject='qiushibaike',body=str,mimetype='text/plain;charset="utf-8"')
+		#print str
+		#settings = get_project_settings()
+		#mailer = MailSender.from_settings(settings)
+		#mailer.send(to='jeremy.h@foxmail.com',subject='qiushibaike',body=str,mimetype='text/plain;charset="utf-8"')
